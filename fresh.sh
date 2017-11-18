34e94ff94be7044fb7e98eb49e45314f60158bcd
Gary Iams <gary@girlfriend.local>
2017-11-18 16:37:03 -0500

    Automate rollback to "Fresh" snapshot
    - Quickest way to try a test bootstrap again

---
+++ fresh.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+osxbox_vmname=`VBoxManage list vms | grep osx | awk '{print $1}' | tr -d '"'`
+
+vagrant halt
+VBoxManage snapshot "$osxbox_vmname" restore Fresh
+vagrant reload
+vagrant up
+vagrant ssh -c 'ln -s /src/etc/bootstrap.sh .'
+
+vagrant ssh
